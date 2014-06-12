4114c83d5bc1f6879644e71329040834d0d778c0
Claudia Hüttenrauch <claudia.huettenrauch@fashionforhome.de>
2014-06-12 17:17:15 +0200

    init commit of DeliveryController

---
+++ app/controllers/DeliveryController.php
@@ -0,0 +1,15 @@
+<?php
+
+class DeliveryController extends BaseController {
+
+	/**
+	 * Display a listing of all active deliveries.
+	 *
+	 * @return Response
+	 */
+	public function getOverviewOfActive()
+	{
+
+	}
+
+}

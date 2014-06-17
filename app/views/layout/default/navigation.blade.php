2c1a53995589bcf93ac0095ce1381d3547f8de04
Claudia Hüttenrauch <claudia.huettenrauch@fashionforhome.de>
2014-06-17 13:23:15 +0200

    back button for small devices

--- app/views/layout/default/navigation.blade.php
+++ app/views/layout/default/navigation.blade.php
@@ -1,13 +1,18 @@
 <div class="navbar navbar-default navbar-fixed-top" role="navigation">
 	<div class="container-fluid">
-		<div class="navbar-header">
+        <div class="navbar-header pull-left">
+            <button type="button" class="pull-left btn btn-default navbar-btn visible-xs visible-sm">
+                <a href="{{{ URL::previous() }}}"><span class="glyphicon glyphicon-arrow-left"></span></a>
+            </button>
+            <a class="navbar-brand" href="#">{{ Config::get('app.project_name') }}</a>
+        </div>
+		<div class="navbar-header pull-right">
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 				<span class="sr-only">Toggle navigation</span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">{{ Config::get('app.project_name') }}</a>
 		</div>
 		<div class="navbar-collapse collapse">
 

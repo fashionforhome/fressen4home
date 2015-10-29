54ae876b5706d5274d93bdedf1f1301941a00b93
Rebel-L <dj@rebel-l.net>
2015-10-29 23:54:41 +0100

    added license header to scripts

--- server.php
+++ server.php
@@ -1,3 +1,20 @@
+/**
+* This is the ..... [Description]
+*
+* @category Main
+* @package Main
+*
+* @author Eduard Bess <eduard.bess@fashion4home.de>
+*
+* @copyright (c) 2014 by fashion4home GmbH <www.fashionforhome.de>
+* @license GPL-3.0
+* @license http://opensource.org/licenses/GPL-3.0 GNU GENERAL PUBLIC LICENSE
+*
+* @version 1.0.0
+*
+* Date: 11.06.2014
+* Time: 22:04
+*/
 <?php
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);

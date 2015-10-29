3051618122ec477c93c5428698a007ba3a294990
Rebel-L <dj@rebel-l.net>
2015-10-29 23:54:25 +0100

    added license header to configs

--- app/config/navigation.php
+++ app/config/navigation.php
@@ -1,5 +1,21 @@
 <?php
-
+/**
+ * This is the ..... [Description]
+ *
+ * @category App
+ * @package Config
+ *
+ * @author Eduard Bess <eduard.bess@fashion4home.de>
+ *
+ * @copyright (c) 2014 by fashion4home GmbH <www.fashionforhome.de>
+ * @license GPL-3.0
+ * @license http://opensource.org/licenses/GPL-3.0 GNU GENERAL PUBLIC LICENSE
+ *
+ * @version 1.0.0
+ *
+ * Date: 16.06.2014
+ * Time: 22:04
+ */
 
 return [
 	'user' => [

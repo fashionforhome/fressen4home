342cdc6947470a5aea4dd89cd669d493f1070cda
Rebel-L <dj@rebel-l.net>
2015-10-30 00:03:42 +0100

    added license header to database

--- app/database/migrations/2015_03_18_213537_NotificationConfigs.php
+++ app/database/migrations/2015_03_18_213537_NotificationConfigs.php
@@ -1,5 +1,22 @@
 <?php
-
+/**
+ * This is the ..... [Description]
+ *
+ * @category App
+ * @package Database
+ * @subpackage Migrations
+ *
+ * @author @author Eduard Bess <eduard.bess@fashion4home.de>
+ *
+ * @copyright (c) 2015 by fashion4home GmbH <www.fashionforhome.de>
+ * @license GPL-3.0
+ * @license http://opensource.org/licenses/GPL-3.0 GNU GENERAL PUBLIC LICENSE
+ *
+ * @version 1.0.0
+ *
+ * Date: 24.03.2015
+ * Time: 22:04
+ */
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
--- app/database/seeds/OrderTableSeeder.php
+++ app/database/seeds/OrderTableSeeder.php
@@ -1,9 +1,22 @@
 <?php
 
 /**
- * @author Claudia Hüttenrauch <claudia.huettenrauch@fashionforhome.de>
- * @copyright Fashion For Home 2014
- * @link http://www.fashionforhome.de
+ * This is the ..... [Description]
+ *
+ * @category App
+ * @package Database
+ * @subpackage Seeds
+ *
+ * @author Claudia Hüttenrauch <claudia.huettenrauch@fashion4home.de>
+ *
+ * @copyright (c) 2014 by fashion4home GmbH <www.fashionforhome.de>
+ * @license GPL-3.0
+ * @license http://opensource.org/licenses/GPL-3.0 GNU GENERAL PUBLIC LICENSE
+ *
+ * @version 1.0.0
+ *
+ * Date: 12.06.2014
+ * Time: 22:04
  */
 class OrderTableSeeder extends DatabaseSeeder
 {
